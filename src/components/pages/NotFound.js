f3ebbd7dc5d1c8cf61e5de8f734766644773c6c3
Devin Ford <devindford@att.net>
2020-10-29 23:21:44 -0400

    Finished desktop version from the course, possibly will refactor for mobile responsiveness later

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className='lead'>The page you are looking for does not exist</p>
+      <br />
+      <p className='text-success text-center large'>¯\_(ツ)_/¯</p>
+    </div>
+  );
+};
+
+export default NotFound;
